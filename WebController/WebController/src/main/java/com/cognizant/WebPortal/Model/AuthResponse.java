8898c3944937ebb7d97db1bc62652b07e7d94060
CDE20IJ012-Ruksar <80447475+CDE20IJ012-Ruksar@users.noreply.github.com>
2021-05-02 12:44:28 +0530

    Add files via upload

---
+++ WebController/WebController/src/main/java/com/cognizant/WebPortal/Model/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.cognizant.WebPortal.Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+	private String uid;
+	private String name;
+	private boolean isValid;
+}
---
+++ WebController/WebController/src/test/java/com/cognizant/WebPortal/Model/AuthResponseTest.java
@@ -0,0 +1,15 @@
+package com.cognizant.WebPortal.Model;
+
+
+import org.junit.jupiter.api.Test;
+
+import com.cognizant.WebPortal.Model.AuthResponse;
+
+class AuthResponseTest {
+
+	@Test
+	void test() {
+		AuthResponse auth=new AuthResponse();
+	}
+
+}
