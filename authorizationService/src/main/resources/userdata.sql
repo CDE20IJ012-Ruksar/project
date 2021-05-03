0ab86bf92ab3801eacb2ef1fc225703d35d46d0d
CDE20IJ012-Ruksar <80447475+CDE20IJ012-Ruksar@users.noreply.github.com>
2021-05-03 11:11:07 +0530

    Delete authorizationService directory

--- authorizationService/src/main/resources/userdata.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO userdata (user_id,user_password,user_name) VALUES ('101','pwd','101');
-INSERT INTO userdata (user_id,user_password,user_name) VALUES ('102','admin1','102');
-INSERT INTO userdata (user_id,user_password,user_name) VALUES ('103','john@007','John');
